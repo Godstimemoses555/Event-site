ebc9b42530085c2ed3bd2def5d2a7b3394be928b
Godstimemoses555 <mosesgodstime334@gnail.com>
2025-10-30 11:04:01 +0100

    i routed a button in sec1 that will route to Loginpages,and add two icons to the confirm button, to display text and hide password,and i added two components to Loginpages,Loginnav,mainpages

--- src/Component/Mainpage.jsx
+++ src/Component/Mainpage.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import hide from "../assets/hide.png"
+import view from "../assets/view.png"
 
 const Mainpage = () => {
-  const [clicl,setClick]=useState(false)
+  const [click,setClick]=useState(false)
   return (
     <div>
       <div className='flex lg:w-[500px] lg:h-[400px] gap-[20px] bg-white rounded-[5px] shadow-white flex-col  items-center justify-center'>
@@ -11,14 +13,48 @@ const Mainpage = () => {
            <input className='flex lg:w-[300px] lg:h-[50px] items-center justify-center  p-[10px]   bg-pink-600 text-white text-[20px]' type="text"  placeholder='Login'/>
         <input  className='flex lg:w-[300px] lg:h-[50px] items-center justify-center  p-[10px]   bg-pink-600 text-white text-[20px] ' type="text"  placeholder='Email or Phone number'/>
         <input  className='flex lg:w-[300px] lg:h-[50px] items-center justify-center p-[10px]  bg-pink-600 text-white text-[20px] ' type="password"  placeholder='Password'/>
+         
 
-          <input  className='flex lg:w-[300px] lg:h-[50px] items-center justify-center p-[10px]  bg-pink-600 text-white text-[20px] ' type="text"  placeholder='Confirm Password'/>
+          <div className='flex  justify-between lg:w-[320px] lg:h-[50px] relative p-2.5 items-center'>
+              <input  className='flex lg:w-[300px] lg:h-[50px] items-center justify-center p-[10px]  bg-pink-600 text-white text-[20px] ' type={click?"text":"password"}
+                placeholder='Confirm Password'  />
+
+                {click?
+
+                (
+
+                <img className='w-[20px] h-[20px] object-contain absolute right-12' 
+                onClick={()=>setClick(!click)}  src={view} alt=""  style={{display:click?"view":"text"}}/>
+                  
+                ):
+
+                (
+
+                <img  className='w-[20px] h-[20px] object-contain absolute right-12'
+              onClick={()=>setClick(!click)} src={hide} alt=""  style={{display:click?"hide":" view"}}/>
+
+                )
+                  
+                }
+
+                
+             
+            
+             
+          </div>
+        
           <Link className='text-[15px] text-pink-400'>
           Don't HAve Account?
           </Link>
 
-            <input  className='flex lg:w-[300px] lg:h-[50px] items-center justify-center p-[10px]  bg-pink-600 text-white text-[20px] ' type="text"  placeholder='Create Account'/>
+         
 
+          <input  className='flex lg:w-[300px] lg:h-[50px] items-center justify-center p-[10px]  bg-pink-600 text-white text-[20px] ' type="text"
+               placeholder='Create Account'/>
+             
+
+
+           
 
 
 
